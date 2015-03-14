f6b6637d3959d7893faa9f4d50c7aafc6b5df12b
sauravtom <stomatrix@gmail.com>
2015-03-14 10:45:55 +0530

    frontend changes in chat UI

--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,39 @@ $( "#send" ).click(function() {
   resp = "foo";
   $(".commentArea").append( "<div class='bubbledLeft'>"+resp+"</div>" );
 });
+
+$("#msg").keyup(function (e) {
+    if (e.keyCode == 13) {
+        // Do something
+        msg = $('#msg').val();
+        $('#msg').val(""); 
+        console.log(msg);
+        $(".commentArea").append( "<div class='bubbledRight'>"+msg+"</div>" );
+
+        var min = 1, max = 3;
+        var delay = Math.floor(Math.random() * (max - min) + min) * 1000;
+        var dots="";
+        setTimeout(
+          function() {
+          	$(".commentArea").append( "<div class='bubbledLeft typing'>typing</div>" );
+          	
+          	window.setInterval(function(){
+          	  n=Math.floor(new Date().getTime() / 1000)%10%4;
+          	  for(i=0;i<n;i++){dots+=".";}
+          	  $(".typing").html( "typing"+dots );
+          	  dots="";
+          	}, 1000);
+            
+          }, delay);
+
+        dots="";
+        var min = 3, max = 8;
+        var delay = Math.floor(Math.random() * (max - min) + min) * 1000;
+        setTimeout(
+          function() {
+            var resp = "Hi Saurav, wassup !!";
+            $(".typing").hide();
+            $(".commentArea").append( "<div class='bubbledLeft'>"+resp+"</div>" );
+          }, delay);
+    }
+});
